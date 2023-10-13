bb9a794716241f610b178085b1b704823e145316
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-10-13 15:38:38 -0700

    adds

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import R from '../../src/assets/R.png'
+import {NavLink} from 'react-router-dom'
 
 
 function NavBar() {
   return (
     <div>
         <nav className='navbar'>
-            <img src={R} alt="logo"/>
-
+            <img src={R} alt="logo" className='logo'/>
+            <div className='desktopMenu'>
+                <NavLink className='desktopMenuList' to='/home'>Home</NavLink>
+                <NavLink className='desktopMenuList' to='/about'>About</NavLink>
+                <NavLink className='desktopMenuList' to='/portfolio'>Portfolio</NavLink>
+                <NavLink className='desktopMenuList' to='/clients'>Clients</NavLink>
+            </div>
+            <button>
+              <img src='' alt='' className='desktopMenuImg' />
+              Contact Me
+            </button>
         </nav>
     </div>
   )
