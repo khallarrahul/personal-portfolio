464fbe1e38621c66570ff936ffcba1b59d4870e1
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-10-13 15:14:12 -0700

    setting up

---
+++ portfolio/src/components/App.js
@@ -0,0 +1,11 @@
+import NavBar from './NavBar'
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import R from '../../src/assets/R.png'
+
+
+function NavBar() {
+  return (
+    <div>
+        <nav className='navbar'>
+            <img src={R} alt="logo"/>
+
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
